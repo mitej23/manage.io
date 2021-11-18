46b7772943d5747e1d750aaf7baadb3ec00fdf67
mitej23 <mitejmadan@gmail.com>
2021-11-18 18:58:10 +0530

    changed the design of landing page

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -3,30 +3,48 @@ import "./Landing.styles.css";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
-  console.log("Landing");
-
   return (
-    <div>
-      <h4>
-        <b>Build</b> a login/auth app with the{" "}
-        <span style={{ fontFamily: "monospace" }}>MERN</span>
-        stack from scratch
-      </h4>
-      <Link to="/register">Register</Link>
-      <br />
-      <br />
-      <Link
-        to="/login"
-        style={{
-          width: "140px",
-          borderRadius: "3px",
-          letterSpacing: "1.5px",
-        }}
-      >
-        Log In
-      </Link>
+    <div className="landing-page">
+      <div className="nav-bar">
+        <div className="landing-logo">
+          <p>Manage.io</p>
+        </div>
+        <div className="nav-links">
+          <p>Features</p>
+          <p>About Me</p>
+        </div>
+      </div>
+      <div className="landing-container">
+        <p className="main-text">Manage your client's</p>
+        <p className="cursive-text">Mutual Funds</p>
+        <div className="landing-buttons">
+          <Link to="/login" className="landing-button slide_down">
+            Log In
+          </Link>
+          <Link to="/register" className="landing-button slide_down">
+            Register
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
+
+/* <Link to="/register">Register</Link>
+        <br />
+        <br />
+        <Link
+          to="/login"
+          style={{
+            width: "140px",
+            borderRadius: "3px",
+            letterSpacing: "1.5px",
+          }}
+        >
+          Log In
+        </Link>
+        <button type="submit" id="submit" className="btn">
+          Add Client
+        </button> */
 
 export default Landing;
