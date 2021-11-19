07914458c39f2001bd4c8b0504361ae6934dc876
mitej23 <mitejmadan@gmail.com>
2021-11-19 13:20:52 +0530

    added navbar

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -2,18 +2,12 @@ import React from "react";
 import "./Landing.styles.css";
 import { Link } from "react-router-dom";
 
+import NavBar from "../../components/NavBar/NavBar";
+
 const Landing = () => {
   return (
     <div className="landing-page">
-      <div className="nav-bar">
-        <div className="landing-logo">
-          <p>Manage.io</p>
-        </div>
-        <div className="nav-links">
-          <p>Features</p>
-          <p>About Me</p>
-        </div>
-      </div>
+      <NavBar />
       <div className="landing-container">
         <p className="main-text">Manage your client's</p>
         <p className="cursive-text">Mutual Funds</p>
@@ -30,21 +24,5 @@ const Landing = () => {
   );
 };
 
-/* <Link to="/register">Register</Link>
-        <br />
-        <br />
-        <Link
-          to="/login"
-          style={{
-            width: "140px",
-            borderRadius: "3px",
-            letterSpacing: "1.5px",
-          }}
-        >
-          Log In
-        </Link>
-        <button type="submit" id="submit" className="btn">
-          Add Client
-        </button> */
 
 export default Landing;
