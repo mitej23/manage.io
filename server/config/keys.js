98f5230f98d1ddc5a3917f7a8fcd744e386a8bd1
mitej23 <mitejmadan@gmail.com>
2022-02-02 23:45:07 +0530

    safety issue solved

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://mitej:xU5hqXIb6B3aFxGH@cluster0.z42lp.mongodb.net/test?retryWrites=true&w=majority",
-  SECRETORKEY: "secret",
-};
-
-//user : mitej
-//xU5hqXIb6B3aFxGH
